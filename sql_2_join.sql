12755be47f5f0f4bfe6c5c886d49daafa9791712
Gentrithotii <Gentrithoti@hotmail.com>
2025-05-08 18:43:09 +0200

    Done in part 2 to exercise 14

--- sql_2_join.sql
+++ sql_2_join.sql
@@ -3,48 +3,82 @@
 
 #
 # 1: Get the cities with a name starting with ping sorted by their population with the least populated cities first
-SELECT *  FROM city AS c WHERE c.Name LIKE "ping%" ORDER BY c.Population ASC;   
+SELECT *  
+FROM city AS c 
+WHERE c.Name LIKE "ping%" 
+ORDER BY c.Population ASC;   
 #
 # 2: Get the cities with a name starting with ran sorted by their population with the most populated cities first
-SELECT *  FROM city AS c WHERE c.Name LIKE "ran%" ORDER BY c.Population DESC;   
+SELECT *  
+FROM city AS c 
+WHERE c.Name LIKE "ran%" 
+ORDER BY c.Population DESC;   
 #
 # 3: Count all cities
-SELECT count(*) AS NumberOfCities FROM city as c;
+SELECT count(*) AS NumberOfCities 
+FROM city as c;
  
 #
 # 4: Get the average population of all cities
-SELECT avg(c.Population) FROM city as c; 
+SELECT avg(c.Population) 
+FROM city as c; 
 #
 # 5: Get the biggest population found in any of the cities
 SELECT max(c.Population)  FROM city as c;
-SELECT c.Name, c.Population FROM city as c WHERE c.Population = (SELECT max(Population) FROM city);
+SELECT c.Name, c.Population 
+FROM city as c 
+WHERE c.Population = (SELECT max(Population) FROM city);
 #
 # 6: Get the smallest population found in any of the cities
 SELECT min(c.Population)  FROM city as c;
 #
 # 7: Sum the population of all cities with a population below 10000
-SELECT sum(c.Population) FROM city AS c WHERE c.population < 10000;
+SELECT sum(c.Population)
+FROM city AS c 
+WHERE c.population < 10000;
 #
 # 8: Count the cities with the countrycodes MOZ and VNM
-SELECT count(c.CountryCode) FROM city AS c WHERE c.CountryCode IN ("MOZ", "VNM");
+SELECT count(c.CountryCode) 
+FROM city AS c 
+WHERE c.CountryCode IN ("MOZ", "VNM");
 #
 # 9: Get individual count of cities for the countrycodes MOZ and VNM
-SELECT c.CountryCode,  COUNT(*) AS City_Count FROM city AS c WHERE c.CountryCode IN ("MOZ", "VNM")  GROUP BY c.CountryCode;
+SELECT c.CountryCode,  COUNT(*) AS City_Count 
+FROM city AS c 
+WHERE c.CountryCode IN ("MOZ", "VNM")
+GROUP BY c.CountryCode;
 #
 # 10: Get average population of cities in MOZ and VNM
-SELECT AVG(c.Population) AS Average_Population FROM city AS c WHERE c.CountryCode IN ("MOZ", "VNM");
+SELECT AVG(c.Population) AS Average_Population
+ FROM city AS c
+ WHERE c.CountryCode IN ("MOZ", "VNM");
 #
 # 11: Get the countrycodes with more than 200 cities
-#
+SELECT  c.CountryCode , COUNT(*)   as amount_of_cities 
+FROM city AS c 
+GROUP BY c.CountryCode 
+HAVING COUNT(*) >200;   
 #
 # 12: Get the countrycodes with more than 200 cities ordered by city count
+SELECT  c.CountryCode , COUNT(*)   AS amount_of_cities
+ FROM city AS c 
+ GROUP BY c.CountryCode 
+ HAVING COUNT(*) >200 
+ ORDER BY count(c.CountryCode) DESC;  
 #
 #
 # 13: What language(s) is spoken in the city with a population between 400 and 500 ?
-#
+SELECT c.Name, cl.Language, c.Population
+FROM city as c 
+JOIN countrylanguage AS cl ON c.CountryCode =  cl.CountryCode
+WHERE c.Population BETWEEN 400 AND 500;
+
 #
 # 14: What are the name(s) of the cities with a population between 500 and 600 people and the language(s) spoken in them
-#
+SELECT c.Name, cl.Language, c.Population
+FROM city as c 
+JOIN countrylanguage AS cl ON c.CountryCode =  cl.CountryCode
+WHERE c.Population BETWEEN 500 AND 600;
 #
 # 15: What names of the cities are in the same country as the city with a population of 122199 (including the that city itself)
 #
